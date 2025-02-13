e53d20971cb4e77edbb33466870c3b808c184db9
Ioffi <Ioffi@KINXY>
2025-02-13 04:07:02 +0000

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("God2Iso - V2")]
+[assembly: AssemblyDescription("Convert Xbox 360 Games on Demand (GOD) Folders to ISO With Ease")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Canonical")]
+[assembly: AssemblyProduct("God2Iso - v2")]
+[assembly: AssemblyCopyright("Copyright © Canonical  2024-2025")]
+[assembly: AssemblyTrademark("Canonical  2024-2025")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e28f677d-3e2b-4a7c-8555-cc899c50a138")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
